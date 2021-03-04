33e8c3327b697b187e21d85cd83fe2ab131472f3
Salma Alfawal <70548082+susalulmumaO12@users.noreply.github.com>
2021-03-04 21:11:27 +0200

    Create ToUpperLowerCase text converter.cpp

---
+++ ToUpperLowerCase text converter.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+#include <cctype>
+
+using namespace std;
+int main(){
+   string s;                   //defining the text string
+cout<<"\nENTER YOUR TEXT\n";
+getline(cin,s);                //getting text from user with whitespaces
+ int n = s.length();
+cout<<"\n\nyour text in meme font is:\n";    //by meme font i mean lowercase with uppercase in the same time
+
+for(int i=0;i<n;i++){
+     char m = s[i];                  //a variable m that changes for each letter of the string
+     if(i%2==0)                 //for even letters in the string 
+     cout<<static_cast<char>(tolower(m));  //convert to lowercase
+     else                       //for odd letters in the string
+     cout<<static_cast<char>(toupper(m));  //convert to uppercase
+
+}
+cout<<endl;
+    return 0;
+}
