9bc7dd613fc9da763f7556dacaffeb83db7e3e85
fawal.salma@gmail.com <fawal.salma@gmail.com>
2024-11-09 12:57:39 +0300

    main

---
+++ Brainy_Warrior/src/main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <fstream>
+#include <nlohmann/json.hpp>
+#include "./structure/tile.h"
+#include "./structure/board.h"
+
+using json = nlohmann::json;
+
+using namespace std;
+
+void printTile(int value) {
+    switch (value) {
+        case 0: cout << "\033[46m0\033[0m "; break;  // 0:sea
+        case 1: cout << "\033[42m1\033[0m "; break;  // 1:land
+        case 4: cout << "\033[103m4\033[0m "; break;  // 4:wall
+        case 5: cout << "\033[35m5\033[0m "; break;  // 5:target
+        case 6: cout << "\033[48;5;226m*\033[0m "; break;  // 6:star
+        case 9: cout << "\033[31m9\033[0m "; break;  // 9:player
+        default: cout << value << " "; break;
+    }
+}
+
+int main() {
+    ifstream inputFile("../src/levels.json");
+    if (!inputFile.is_open()) {
+        cerr << "Error opening levels.json file!" << endl;
+        return 1;
+    }
+
+    // create an empty structure (null)
+    json j;
+    inputFile >> j;
+
+    // get user input for level number
+    int levelChoice;
+    cout << "Enter a level number between 1 and 15: ";
+    cin >> levelChoice;
+
+    // is level number valid?
+    if (levelChoice < 1 || levelChoice > 15) {
+        cerr << "Invalid level number!" << endl;
+        return 1;
+    }
+
+    // construct a string with level name "level i"
+    string levelName = "level " + to_string(levelChoice);
+
+    // does level exist?
+    if (j.find(levelName) == j.end()) {
+        cerr << "Level " << levelChoice << " not found!" << endl;
+        return 1;
+    }
+
+    // level data from json file
+    auto& levelData = j[levelName];
+    int n = levelData.size();
+    int m = levelData[0].size();
+
+    // create board from json data
+    Board board(n, m);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            board.getTile(i, j).setValue(levelData[i][j]);
+        }
+    }
+
+    cout << "Board for " << levelName << ":\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printTile(board.getTile(i, j).getValue());
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
--- Brainy_Warrior/src/structure/tile.cpp
+++ Brainy_Warrior/src/structure/tile.cpp
@@ -4,6 +4,7 @@ Tile::Tile(int row, int col, int value) {
     this->row = row;
     this->col = col;
     this->value = value;
+    //0:sea, 1:land, 4:wall, 5:target, 6:star, 9:player
 }
 
 int Tile::getValue() {
