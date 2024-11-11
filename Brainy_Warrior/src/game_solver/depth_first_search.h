61c08da3c5ead56ae65f51ff577413b5c2b2564d
fawal.salma@gmail.com <fawal.salma@gmail.com>
2024-11-11 22:42:52 +0300

    DFS worked successfully

--- Brainy_Warrior/src/game_solver/depth_first_search.h
+++ Brainy_Warrior/src/game_solver/depth_first_search.h
@@ -1,35 +1,40 @@
+#ifndef DEPTH_FIRST_SEARCH_H
+#define DEPTH_FIRST_SEARCH_H
+
 #include<iostream>
 #include<list>
 #include<stack>
 #include"../structure/board.h"
-#include"../structure/tile.h"
 #include"../include/main.h"
+#include"algorithm_helper.h"
 
 
 void dfs(Board board){
 
     stack<Board> s;
     s.push(board);
+    list<Board> visitedStates;
 
-    bool win = false;
-    while(!win || !s.empty()){
+    while(!s.empty()){
 
         Board current = s.top();
-        win = current.win();
-        Tile& player = board.getPlayerTile();
-
-
         printBoard(current);
         s.pop();
 
-        list<Board> states = get_next_states(current, player);
-        for(auto state:states){
-            s.push(state);
-        }
-
-        if(win){
+        if(current.win()){
             cout<<"\033[38;5;226mYOU WIN!\033[0m\n";
             exit(0);
         }
+
+        list<Board> states = get_next_states(current);
+        for(auto state:states){
+
+            if(!isVisited(visitedStates, state)){
+                visitedStates.push_back(state);
+                s.push(state);
+            }
+        }
     }
 }
+
+#endif
