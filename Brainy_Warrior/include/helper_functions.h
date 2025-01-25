51236426ee44bdb5d88744133150c45ad76e8248
susalulmumaO12 <fawal.salma@gmail.com>
2025-01-25 21:29:22 +0300

    move

--- Brainy_Warrior/include/helper_functions.h
+++ Brainy_Warrior/include/helper_functions.h
@@ -11,14 +11,12 @@
 #include "main.h"
 #include "moves.h"
 
-using namespace std;
-
 Board set_game_board(int levelChoice);
 void printScores(int level);
-void updateStats(const string& playerName, int level, bool win);
-list<Node_State> get_next_cost_states(Node_State* board);
+void updateStats(const std::string& playerName, int level, bool win);
+std::list<Node_State> get_next_cost_states(Node_State* board);
 int distance(Tile t1, Tile t2);
 Board getPath(Node_State* win);
-string stringBoard(Board& b);
+std::string stringBoard(Board& b);
 
 #endif /* HELPER_FUNCTIONS.H */
